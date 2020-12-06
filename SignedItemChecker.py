f16b295f5947d92cb67cb9056398820bf80c400d
DanielSalas236 <cdanielsalas236@hotmail.com>
2020-12-06 16:10:49 -0500

    First

--- SignedItemChecker.py
+++ SignedItemChecker.py
@@ -45,4 +45,4 @@ def sendDiscordMessage(URL):
 while True:
     findWord("https://store.taylorswift.com/", "signed")
     findWordClass("https://shop.arianagrande.com/", "signed", "product-container clearfix", "product-details")
-    time.sleep(30)
+    time.sleep(1800)
